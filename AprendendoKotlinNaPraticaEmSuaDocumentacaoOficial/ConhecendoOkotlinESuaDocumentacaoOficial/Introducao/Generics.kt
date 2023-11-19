e5b2aadb288333d3cf6d4bf11b0f56474eca6aaf
Eduardo Lima <duduzx02@gmail.com>
2023-11-19 04:45:53 -0300

    Generics - Funcões Genéricas

--- AprendendoKotlinNaPraticaEmSuaDocumentacaoOficial/ConhecendoOkotlinESuaDocumentacaoOficial/Introducao/Generics.kt
+++ AprendendoKotlinNaPraticaEmSuaDocumentacaoOficial/ConhecendoOkotlinESuaDocumentacaoOficial/Introducao/Generics.kt
@@ -16,7 +16,7 @@ class MutableStack<E>(vararg items: E){
     override fun toString() = "MutableStack(${elements.joinToString()})"
 }
 
-fun main() {
+ /* fun main() {
     val stack = MutableStack(0.63,  3.14, 2.7)
     println("push(9.00): ${stack.push(9.00)}")
     println("push(7.00): ${stack.push(7.00)}")
@@ -31,4 +31,12 @@ fun main() {
     }
 
     println("isEmpty(): ${stack.isEmpty()}")
+}
+*/
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = mutableStackOf(0.6, 3.14,4.9)
+    println(stack)
 }
