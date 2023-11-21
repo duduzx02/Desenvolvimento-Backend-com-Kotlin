a832fb56b357b2e6dbc8a20d48fadb4b391a4991
Eduardo Lima <duduzx02@gmail.com>
2023-11-21 03:35:06 -0300

    Os intervalors de caracters também funcionam e tbm em intervalors em instruções if

--- AprendendoKotlinNaPraticaEmSuaDocumentacaoOficial/EstruturasDeControleDeFluxoeColecoesEmKotlin/Ranges.kt
+++ AprendendoKotlinNaPraticaEmSuaDocumentacaoOficial/EstruturasDeControleDeFluxoeColecoesEmKotlin/Ranges.kt
@@ -18,4 +18,27 @@ fun main() {
     for(i in 3 downTo 0){
         print(i)
     }
+
+    println()
+    for (a in 'a'..'z'){
+        print(a)
+    }
+
+    println()
+
+    for (a in 'z' downTo 'a'){
+        print(a)
+    }
+
+    println()
+    val x =2
+    if (x in 1..5){
+        print("X está no range de 1 a 5")
+    }
+
+    println()
+
+    if( x !in 6..40){
+       print("X não está no range de 6 a 40")
+    }
 }
