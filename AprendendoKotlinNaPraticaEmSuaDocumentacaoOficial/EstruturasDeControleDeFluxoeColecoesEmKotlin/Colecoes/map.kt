1da0886c37604545a186f54d5c99e7964f866da0
Eduardo Lima <duduzx02@gmail.com>
2023-11-25 01:41:01 -0300

    map

---
+++ AprendendoKotlinNaPraticaEmSuaDocumentacaoOficial/EstruturasDeControleDeFluxoeColecoesEmKotlin/Colecoes/map.kt
@@ -0,0 +1,11 @@
+package Colecoes
+
+val number = listOf(1, -2, 8, -8, -7, 8, 6 )
+val double = number.map { it * 2 }
+val tipled = number.map { it * 3 }
+
+fun main() {
+    println(number)
+    println(double)
+    println(tipled)
+}
